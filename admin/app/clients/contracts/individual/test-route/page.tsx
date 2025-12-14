1a9937ad66dc3341a688fc1fd4373d3c2d63159f
teo_work <shinji.tomonou@teo-inc.com>
2025-12-14 23:27:09 +0900

    Add test route to debug individual folder routing

---
+++ admin/app/clients/contracts/individual/test-route/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+    return (
+        <div className="p-10 text-xl font-bold text-green-600">
+            Individual Route Directory is Accessible!
+        </div>
+    );
+}
